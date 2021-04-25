28ada511f0b337116f4ef3e81ca490d4b3e1ce22
alitariq12369 <alitariq.uet2020@gmail.com>
2021-04-25 05:55:17 +0500

    Task_04_2_Functions
    In this commit, two functions of task 4 has been added.

--- src/pd_week_09/Task3.java
+++ src/pd_week_09/Task3.java
@@ -13,88 +13,84 @@ import java.util.Base64;
  * @author Ali tariq
  */
 public class Task3 {
-    public static void main(String args[]){
-        String s="";
-      s=JOptionPane.showInputDialog(null,"\tREMOVE EXTRA BLANKS\n"+"Input String :");
-      String output= ridMultipleBlank(s);
-      JOptionPane.showMessageDialog(null,output);
 
+    public static void main(String args[]) {
+        String s = "";
+        s = JOptionPane.showInputDialog(null, "\tREMOVE EXTRA BLANKS\n" + "Input String :");
+        String output = ridMultipleBlank(s);
+        JOptionPane.showMessageDialog(null, output);
 
+        s = JOptionPane.showInputDialog(null, "\tREMOVE INTEGERS From STRING Conflictts \n" + "Input String :");
 
-      s=JOptionPane.showInputDialog(null,"\tREMOVE INTEGERS From STRING Conflictts \n"+"Input String :");
+        output = removeInteger(s);
+        JOptionPane.showMessageDialog(null, output);
+        s = JOptionPane.showInputDialog(null, "\tEncryption String\n" + "Input String :");
+        output = stringEncryption(s);
+        JOptionPane.showMessageDialog(null, "Encrypted String is :\n" + output);
 
-
-          output= removeInteger(s);
-      JOptionPane.showMessageDialog(null,output);
-     s=JOptionPane.showInputDialog(null,"\tEncryption String\n"+"Input String :");
-          output= stringEncryption(s);
-      JOptionPane.showMessageDialog(null,"Encrypted String is :\n"+output);
-                
-      
-      
     }
     //*********************************ridMultipleBlankRemover******************************
-    
-     /**
+
+    /**
      * Remove extra spaces from the String
+     *
      * @param s
      * @return s
      */
-    public static String ridMultipleBlank(String s){
-       for(int i= 0 ; i< s.length();i++)
-       {
-           //JOptionPane.showMessageDialog(null,"check 1");
-           if(s.charAt(i)==' '&& s.charAt(i+1)==' ')
-           {
-               
-             s = s.replace("  ", " ");
-        
-           }
-            
-       }
-    return s;
-}
-    //*********************************REMOVE INTEGER******************************
-    
-     /**
-     * Remove Integer from the String
-     * @param s
-     * @return s
-     */
-    public static String removeInteger(String s){
-        for(int i=0;i< s.length();i++){
-        s = s.replace("0", "");
-        s = s.replace("1", "");
-        s = s.replace("2", "");
-        s = s.replace("3", "");
-        s = s.replace("4", "");
-        s = s.replace("5", "");
-        s = s.replace("6", "");
-        s = s.replace("7", "");
-        s = s.replace("8", "");
-        s = s.replace("9", "");
-        
+    public static String ridMultipleBlank(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            //JOptionPane.showMessageDialog(null,"check 1");
+            if (s.charAt(i) == ' ' && s.charAt(i + 1) == ' ') {
+
+                s = s.replace("  ", " ");
+
+            }
+
         }
         return s;
     }
-        //*********************************Encryption of String******************************
-    
-     /**
+    //*********************************REMOVE INTEGER******************************
+
+    /**
+     * Remove Integer from the String
+     *
+     * @param s
+     * @return s
+     */
+    public static String removeInteger(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            s = s.replace("0", "");
+            s = s.replace("1", "");
+            s = s.replace("2", "");
+            s = s.replace("3", "");
+            s = s.replace("4", "");
+            s = s.replace("5", "");
+            s = s.replace("6", "");
+            s = s.replace("7", "");
+            s = s.replace("8", "");
+            s = s.replace("9", "");
+
+        }
+        return s;
+    }
+    //*********************************Encryption of String******************************
+
+    /**
      * Encryption of String with key of 10th next character
+     *
      * @param s
      * @return encryptedString
      */
-    public static String stringEncryption(String s){
+    public static String stringEncryption(String s) {
         int key = 10; //increase every letter with 10th next character in asscii table
-        String encryptedString=""; //returnable String
-        String adder="";//helping string
+        String encryptedString = ""; //returnable String
+        String adder = "";//helping string
         char[] encrypt = s.toCharArray();   //Array to place every single character after encoding
-        for(char encrypted : encrypt ){
+        for (char encrypted : encrypt) {
             encrypted += key;
-             adder = Character.toString(encrypted); //converting into String 
-             encryptedString=encryptedString+adder;
+            adder = Character.toString(encrypted); //converting into String 
+            encryptedString = encryptedString + adder;
         }
         return encryptedString;
     }
 }
- 
--- src/pd_week_09/Task4.java
+++ src/pd_week_09/Task4.java
@@ -5,10 +5,60 @@
  */
 package pd_week_09;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Hamad DEVIL
  */
 public class Task4 {
-    
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        String input = JOptionPane.showInputDialog("Enter the String");
+
+        JOptionPane.showMessageDialog(null, "Number of Up case Letters: " + noOfUpCaseLetters(input) + "\n"
+                + "Number of Lower case Letters: " + noOfLowerCaseLetters(input) + "\n");
+
+    }
+
+    /**
+     * This function returns the number of all up case letters in the string.
+     *
+     * @param s is a string which is passed to the function.
+     * @return count variable
+     */
+    public static int noOfUpCaseLetters(String s) {
+
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) >= 'A' && s.charAt(i) <= 'Z') {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    /**
+     * This function returns the number of all lower case letters in the string.
+     *
+     * @param s is a string which is passed to the function.
+     * @return count variable.
+     */
+    public static int noOfLowerCaseLetters(String s) {
+
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) >= 'a' && s.charAt(i) <= 'z') {
+                count++;
+            }
+        }
+
+        return count;
+    }
 }
